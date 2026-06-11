003b0ff74be367322859ead262549a2968221d2e
Geddo <holif9859@gmail.com>
2026-06-11 11:50:18 +0300

    Fix all seeders

--- database/seeders/WarehouseSeeder.php
+++ database/seeders/WarehouseSeeder.php
@@ -9,19 +9,14 @@ class WarehouseSeeder extends Seeder
 {
     public function run(): void
     {
-        Warehouse::insert([
-            [
-                'name'       => 'Головний склад',
-                'address'    => 'вул. Складська, 1',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name'       => 'Склад №2',
-                'address'    => 'вул. Промислова, 15',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ]);
+        Warehouse::firstOrCreate(
+            ['name' => 'Головний склад'],
+            ['address' => 'вул. Складська, 1']
+        );
+
+        Warehouse::firstOrCreate(
+            ['name' => 'Склад №2'],
+            ['address' => 'вул. Промислова, 15']
+        );
     }
 }
