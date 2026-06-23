1fce5173b305a1993bf3fb72fad21192822c1b30
Geddo <holif9859@gmail.com>
2026-06-23 19:21:17 +0300

    Add CSP tag to fix Livewire HTTP requests

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 <title>
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
